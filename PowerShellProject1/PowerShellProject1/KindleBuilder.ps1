31c1510aab66d32b1452947641cbc33eea940f39
Zachary Cox <zacharycox@tamu.edu>
2017-02-19 13:08:45 -0600

    Initial commit

---
+++ PowerShellProject1/PowerShellProject1/KindleBuilder.ps1
@@ -0,0 +1,13 @@
+﻿#Global Variables
+$InputFormat #Format Pandoc will expect for input
+$OutputFormat #Format Pandoc will expect for output
+$MetaDataFile #File generated with metadata
+$OutputFile #Filepath and file of the end ebook
+$InputFiles #A string concatonated to be added at end of pandoc command
+
+#Introduction stuff
+
+#Open a window to allow user to select output directory
+
+#Conversion Command
+pandoc -f $InputFormat --epub-metadata=$MetaDataFile -o $OutputFile $InputFiles
